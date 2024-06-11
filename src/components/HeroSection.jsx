9a128a96bbe50f4428e99761b2765781637cfed4
Théodorebinda <theodorebinda@gmail.com>
2024-06-11 02:19:53 +0100

    fix

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,24 +2,31 @@ import ImageComponent from "./ImagesComponent";
 
 export default function HeroSection() {
   return (
-    <div id="hero" className={` relative min-h-screen pt-2`}>
+    <div id="hero" className={` relative min-h-screen pt-2 text-[#181818]`}>
       <div className="absolute inset-0 bg-custom z-0"></div>
-      <div className="flex justify-between items-center mx-10 relative z-10">
-        <div>
-          <span className="text-shadow-md">Hello word! Je suis</span>
-          <div>
-            <h1 className="text-5xl font-bold  text-shadow-md">
-              Theodore Samba
-            </h1>
-            <span className="text-xl   text-shadow-md">
-              Developpeur Web et Mobile |
-              <br />
-              UI/UX Designer
-            </span>
+      <div className="flex justify-between items-center  mx-10 relative z-10">
+        <div className="flex justify-between items-center flex-col gap-20">
+          <div className="mt-12">
+            <span className="text-shadow-md pl-3">Hello word! Je suis</span>
+            <div className="flex flex-col justify-center items-start gap-4">
+              <h1 className="text-7xl font-bold  text-shadow-md">
+                Theodore Samba
+              </h1>
+              <span className="text-xl   text-shadow-md pl-3">
+                Developpeur Web et Mobile |
+                <br />
+                UI/UX Designer
+              </span>
+            </div>
           </div>
-          <div className="flex justify-center gap-5">
-            <button className="border py-2 px-4 shadow-lg">Contact</button>
-            <button className="border py-2 px-4 shadow-lg">Mon CV</button>
+
+          <div className="flex justify-center gap-10 ">
+            <button className="rounded-md py-3 px-8 shadow-lg bg-[#c1bebe]">
+              Contact
+            </button>
+            <button className="rounded-md  py-3 px-8 shadow-lg bg-[#c1bebe]">
+              Mon CV
+            </button>
           </div>
         </div>
         <div className="">
